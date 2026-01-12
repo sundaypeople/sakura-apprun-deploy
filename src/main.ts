6768c2ea1afcd55dfad3f3093ff7d478bfc8f3bf
yakitate ja pan <flx503@outlook.jp>
2026-01-12 14:45:14 +0900

    refactoring not to use class

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,14 @@
 import { setOutput, setFailed } from '@actions/core';
-import { ApprunClient } from './apprun-client';
+import { apprunClient, getAllApplication, patchPacketFilter, createApplication, getApplication, patchApplication } from './apprun-client';
 import { getAPIKey, getConfig, replaceEnv } from './actions-configration';
 import * as model from './model';
 
 export async function run(): Promise<void> {
   try {
     const keys = getAPIKey();
-    const client = new ApprunClient(keys.token, keys.secret);
+    const client = apprunClient(keys.token, keys.secret);
 
-    const applications = await client.getAllApplication();
+    const applications = await getAllApplication(client);
     const nameToIdMap = new Map<string, string>();
     for (const data of applications.data) {
       nameToIdMap.set(data.name, data.id);
@@ -16,23 +16,23 @@ export async function run(): Promise<void> {
     const [application, packetFilter, inheritEnv] = getConfig(nameToIdMap);
     let publicURL = '';
     if (!('id' in application)) {
-      const result = await client.createApplication(application as model.CreateApplicationRequest);
+      const result = await createApplication(client, application as model.CreateApplicationRequest);
       console.log('create application:\n', JSON.stringify(result, null, 2));
-      const resultPacketfilter = await client.patchPacketFilter(result.id, packetFilter);
-      console.log('patch packet filter:\n', JSON.stringify(resultPacketfilter, null, 2));
+      const resultPacketFilter = await patchPacketFilter(client, result.id, packetFilter);
+      console.log('patch packet filter:\n', JSON.stringify(resultPacketFilter, null, 2));
       publicURL = result.public_url;
     } else {
       let sendAppParam = application;
       if (inheritEnv) {
         if (typeof application.id === 'string') {
-          const result = await client.getApplication(application.id);
+          const result = await getApplication(client, application.id);
           sendAppParam = replaceEnv(application, result);
         }
       }
-      const result = await client.patchApplication(sendAppParam);
+      const result = await patchApplication(client, sendAppParam);
       console.log('update application:\n', JSON.stringify(result, null, 2));
-      const resultPacketfilter = await client.patchPacketFilter(result.id, packetFilter);
-      console.log('patch packet filter:\n', JSON.stringify(resultPacketfilter, null, 2));
+      const resultPacketFilter = await patchPacketFilter(client, result.id, packetFilter);
+      console.log('patch packet filter:\n', JSON.stringify(resultPacketFilter, null, 2));
       publicURL = result.public_url;
     }
     setOutput('public_url', publicURL);
